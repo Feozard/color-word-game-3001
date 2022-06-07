8694c05c990a336a45426c1f78b33f09c712f833
Feozard <104563112+Feozard@users.noreply.github.com>
2022-06-07 14:22:40 +0200

    Update game.js

--- game.js
+++ game.js
@@ -272,7 +272,7 @@ const countryList = [
 
   
 var countryListWithoutCountryToFind;
-var numberWords = 100;
+var numberWords = 2000;
 var buttonCountryList = [];
 var countryToFind;
 var countryIndex;
